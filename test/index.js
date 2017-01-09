c3b8a245e776edd53de6c3da347812784f5f9057
Máté Szurovecz <szuri92@gmail.com>
2017-01-09 13:42:57 +0100

    add some test

--- test/index.js
+++ test/index.js
@@ -3,9 +3,30 @@
 var test = require('tape');
 var dcrypt = require('./../decrypt.js')
 
+test('decrypt abc by 1', function (t) {
+    var actual = dcrypt.decrypt('abc', 1);
+    var expected = 'bcd';
+    t.equal(actual, expected);
+    t.end();
+});
+
 test('decrypt simple text', function (t) {
-    var actual = decrypter('bcd');
+    var actual = dcrypt.decrypt('bcd', -1);
     var expected = 'abc';
+    t.equal(actual, expected);
+    t.end();
+});
+
+test('negative overflow', function (t) {
+    var actual = dcrypt.decrypt('abc', -1);
+    var expected = 'zab';
+    t.equal(actual, expected);
+    t.end();
+});
+
+test('positive overflow', function (t) {
+    var actual = dcrypt.decrypt('xyz', 1);
+    var expected = 'yza';
     t.equal(actual, expected);
     t.end();
 });
